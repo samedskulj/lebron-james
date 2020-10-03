72c326e92160e0e7aec496b965c949eb8961829d
Samed Skulj <63544749+samedskulj@users.noreply.github.com>
2020-10-03 23:52:05 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const hamburger = document.querySelector(".header .nav-bar .nav-list .hamburger");
 const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const lista = document.querySelector(".header .nav-bar .nav-list li");
 const header = document.querySelector(".header.container");
 const instagram = document.querySelector("#instagram");
 const facebook = document.querySelector(".facebook");
@@ -8,7 +9,14 @@ const twitter = document.querySelector("twitter");
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active")
     mobile_menu.classList.toggle("active")
+    
 })
+
+mobile_menu.addEventListener("click", () => {
+    hamburger.classList.toggle("active")
+    mobile_menu.classList.toggle("active")
+})
+
 
 document.addEventListener("scroll", ()=> {
     let scroll_position = window.scrollY;
