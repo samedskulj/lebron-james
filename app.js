e645baa3792aecf4ea51ae66130f0f1ff1aaaaca
Samed Skulj <63544749+samedskulj@users.noreply.github.com>
2020-10-03 18:15:50 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector(".header .nav-bar .nav-list .hamburger");
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const header = document.querySelector(".header.container");
+const instagram = document.querySelector("#instagram");
+const facebook = document.querySelector(".facebook");
+const twitter = document.querySelector("twitter");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active")
+    mobile_menu.classList.toggle("active")
+})
+
+document.addEventListener("scroll", ()=> {
+    let scroll_position = window.scrollY;
+    if(scroll_position > 250) {
+        header.style.backgroundColor = "#29323c";
+    }
+    else {
+        header.style.backgroundColor = "transparent";
+    }
+})
